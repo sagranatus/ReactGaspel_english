d8c6b2e6172cec0f8ba12ee3e26a157aadf7937e
장새아 <jangsaea@jangsaeas-Mac.local>
2019-05-03 17:47:39 +0900

    change sdk root

--- pages/MainPage.js
+++ pages/MainPage.js
@@ -17,7 +17,7 @@ import FirstPage from '../containers/FirstPageContainer';
 import LoginUser from '../containers/LoginUserContainer';
 import { TextInput } from 'react-native-gesture-handler';
 
-//saea git test
+//saea only change sdk root
 const getTabBarIcon = (navigation, focused, tintColor) => {
 	const { routeName } = navigation.state;
 	let IconComponent = Icon;
